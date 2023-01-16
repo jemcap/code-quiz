a3e0de4b8c5e4f3dcdbf01440b60892bea73c9d1
JCapito <joshuaecapito22@gmail.com>
2023-01-16 18:20:03 +0000

    Added a function that lets the user proceed to the next question when they choose their answer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,42 +3,32 @@
 // * A start button that when clicked a timer starts and the first question appears.
 // How to start timer when button is clicked?
 
-let startScreen = document.querySelector('#start-screen');
+let startScreen = document.querySelector("#start-screen");
 
 // Grab button
 let startBtn = document.querySelector("#start");
 // Grab timer
 let timeContent = document.querySelector(".timer");
 let timeClock = document.querySelector("#time");
-let quizEl = document.querySelector('#questions');
-let qTitle = document.querySelector('#question-title');
-let choiceA = document.querySelector('#a');
-let choiceB = document.querySelector('#b');
-let choiceC = document.querySelector('#c');
-let choiceD = document.querySelector('#d');
-
-let lastQuestionIndex = questions.length - 1;
-let runningQuestionIndex = 0;
-
-function renderQ() {
-  let q = questions[runningQuestionIndex];
-    qTitle.innerHTML = q.question;
-    choiceA.innerHTML = q.a;
-    choiceB.innerHTML = q.b;
-    choiceC.innerHTML = q.c;
-    choiceD.innerHTML = q.d;
-}
+let quizEl = document.querySelector("#questions");
+let qTitle = document.querySelector("#question-title");
+let choiceA = document.querySelector("#a");
+let choiceB = document.querySelector("#b");
+let choiceC = document.querySelector("#c");
+let choiceD = document.querySelector("#d");
 
 
+let timer = 75;
+
+// When the start button is clicked, it must hide the starting content and display the questions on screen
 function startQuiz() {
-  startBtn.addEventListener("click", function() {
+  startBtn.addEventListener("click", function () {
     startScreen.style.display = "none";
     renderQ();
     quizEl.style.display = "block";
-    
+
     // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    let timer = 75;
-    var timeInterval = setInterval(function() {
+    var timeInterval = setInterval(function () {
       timer--;
       timeClock.textContent = timer;
 
@@ -55,16 +45,58 @@ startQuiz();
 // How to navigate to the first question at the same time the button is clicked?
 // It should transition to the question when the button is clicked and the timer starts to count down
 
+let lastQuestionIndex = questions.length - 1;
+let runningQuestion = 0;
+
+// Manipulate the DOM so that the content is displayed on the screen from the objects in the arrays in questions.js
+
+function renderQ() {
+  let q = questions[runningQuestion];
+  qTitle.innerHTML = q.question;
+  choiceA.innerHTML = q.a;
+  choiceB.innerHTML = q.b;
+  choiceC.innerHTML = q.c;
+  choiceD.innerHTML = q.d;
+}
 
 
-// The questions should be stored in an array with the answer options 
+function checkAnswer(answer) {
+  if (questions[runningQuestion].correct == answer) {
+    answerCorrect();
+  } else {
+    answerWrong();
+  }
+  if (runningQuestion < lastQuestionIndex) {
+    runningQuestion++;
+    renderQ();
+  }
+}
+
+checkAnswer();
+
+function answerCorrect() {
+  let correctSfx = new Audio("../sfx/correct.wav");
+  correctSfx.play();
+}
+
+function answerWrong() {
+  let incorrectSfx = new Audio("../sfx/incorrect.wav");
+  incorrectSfx.play();
+  timer -= 10;
+}
+
+function finalScore() {
+
+
+
+}
+
+// The questions should be stored in an array with the answer options
 
 // The data from the array should display to the webpage.
 // The answer option(s) must also appear under the question for the user to answer
 
 // How to display whether the chosen answer was correct or incorrect?
-
-
 
 //   * Questions contain buttons for each answer.
 //   *
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -15,5 +15,45 @@ const questions = [
         d: "Meta tags show the user which window is running the code",
         correct: "a"
     },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        a: "onchange",
+        b: "onclick",
+        c: "onpress",
+        d: "onmouseclick",
+        correct: "b",
+    },
+    {
+        question: "How does a FOR loop start?",
+        a: "for i = 1 to 5",
+        b: "for (i <= 5; i++)",
+        c: "for (i = 0; i <= 5; i++)",
+        d: "for (i = 0; i < 5)",
+        correct: "c"
+    },
+    {
+        question: "What is the correct way of rounding the number 5.25 to the nearest integer?",
+        a: "Math.round(5.25)",
+        b: "Math.rnd(5.25)",
+        c: "Math.roundNear(5.25)",
+        d: "Math.round.int(5.25)",
+        correct: "a"
+    },
+    {
+        question: "What are meta  in HTML?",
+        a: "Meta tags consist of character encoding, title, or description",
+        b: "Meta tags store the main content of the webpage",
+        c: "Meta tags are specific IDs that hold the link of the document",
+        d: "Meta tags show the user which window is running the code",
+        correct: "a"
+    },
+    {
+        question: "What are meta tags in HTML?",
+        a: "Meta tags consist of character encoding, title, or description",
+        b: "Meta tags store the main content of the webpage",
+        c: "Meta tags are specific IDs that hold the link of the document",
+        d: "Meta tags show the user which window is running the code",
+        correct: "a"
+    },
 ]
 
